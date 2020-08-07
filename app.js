8717e81c44ea9d8ebb227a38097f16d23059495d
Daphnee <daphneejeune.dev@gmail.com>
2020-08-06 23:02:28 -0400

    process.env.PORT

--- app.js
+++ app.js
@@ -26,4 +26,4 @@ app.get('/', (req, res) => {
     res.send("Hello from backend app.js")
 })
 
-app.listen(5000, () => console.log("Connected on Port 5000"))
+app.listen(process.env.PORT || 5000, () => console.log("Connected on Port 5000"))
